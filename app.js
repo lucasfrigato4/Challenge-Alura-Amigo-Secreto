025522dc4b0243a350837aeb6746ec54bfcc6a61
Lucas Frigato <lucasfrigato4@gmail.com>
2025-02-07 18:11:27 -0300

    proj final

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
+var amigos = [];
 
+function agregarAmigos() {
+    let input = document.getElementById('amigo');
+    let nome = input.value;
+
+
+    if (nome != '') {
+        amigos.push(nome);
+        console.log(amigos);
+        input.value = '';
+        mostrarNomesNaTela();
+
+    } else {
+        alert('Adicione um nome válido');
+    }
+}
+
+function mostrarNomesNaTela() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    amigos.forEach(function(nome) {
+        lista.innerHTML += `<li>${nome}</li>`;
+    });
+}
+
+function sortearAmigo() {
+    let indiceAleatorio = Math.floor(Math.random()* amigos.length);
+    let nomeAleatorio = amigos[indiceAleatorio];
+    let nomeQueaparece = document.getElementById('listaAmigos');
+    
+    
+    if (amigos.length == 0) {
+        alert ('adicione amigos antes de sortear');
+    } else {
+        nomeQueaparece.innerHTML = `<li>${nomeAleatorio}</li>`;
+    }
+}
